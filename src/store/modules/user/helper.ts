2a37f09cd809fd89c9ac0a26862456a33e045d61
ok5288 <130146065+ok5288@users.noreply.github.com>
2024-12-11 17:12:29 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'http://www.ok5266.com/ai/src/assets/avatar2.jpg',
+      name: 'Edward AI Chat',
+      description: 'Homepage: <a href="http://www.ok5266.com" class="text-blue-500" target="_blank" >OK5266</a>',
     },
   }
 }
